78ff57032badc9b25f1e37ab4447f84ecefdea1b
GoPJo <jonas.schindler@stud-mail.uni-wuerzburg.de>
2021-05-03 10:40:59 +0200

    Include example formula from lecture

--- src/bresenham.ts
+++ src/bresenham.ts
@@ -38,6 +38,32 @@ export function bresenham(data: Uint8ClampedArray, pointA: [number, number], poi
             pointAy += sY
         }
     }
+
+    let point1x: number = 20
+    let point1y: number = 20
+    let point2x: number = 280
+    let point2y: number = 280
+
+    let currentX: number = point1x
+    let currentY: number = point1y
+
+    let steps: number = Math.abs(point2x-point1x)
+
+    let dX1: number = point2x-point1x
+    let dY1: number = point2y-point1y
+
+    let err: number = 2 * dY - dX
+
+    for(let i: number = 0; i < steps; i++){
+        currentX += 1
+        if(err > 0){
+            currentY += 1 
+            err += 2*dY1 - 2*dX1
+        } else {
+            err += 2*dY1
+        }
+        pixelSet(currentX, ConvertY(currentY, height), width, data)
+    }
 }
 
 function pixelSet(x: number, y: number, width: number, data: Uint8ClampedArray){
