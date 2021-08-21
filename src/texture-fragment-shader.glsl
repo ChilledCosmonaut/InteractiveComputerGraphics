b6355bab1d5671977e03e7c2f36ed1533bf53f95
Jonas <jonas.schindler@stud-mail.uni-wuerzburg.de>
2021-08-21 14:39:27 +0900

    Introduced Phong logic with applied normal map

--- src/texture-fragment-shader.glsl
+++ src/texture-fragment-shader.glsl
@@ -1,12 +1,36 @@
 precision mediump float;
 
 uniform sampler2D sampler;
+
 varying vec2 v_texCoord;
+varying vec2 v_normalCoord;
+varying vec3 vertexPosition;
+
+const vec3 lightPos = vec3(1.0, 1.0, 1.0);
+const float shininess = 16.0;
+const float kA = 0.3;
+const float kD = 0.6;
+const float kS = 0.7;
 
 void main(void) {
   //gl_FragColor = vec4(0.0, 0.0, 0.5, 1.0);
-  gl_FragColor = texture2D(sampler, v_texCoord);
-  gl_FragColor.a = 1.0;
+  vec3 color = texture2D(sampler, v_texCoord);
+  color.a = 1.0;
+
+  vec3 normal = texture2D(sampler, v_normalCoord);
   // Read fragment color from texture
   // TODO
+
+  vec3 ambientColor = color * kA;
+
+  vec3 toLightVector = normalize(lightPos - vertexPosition);
+  vec3 diffuseColor = color * kD * max ( 0.0, dot ( normal ,toLightVector ));
+
+  vec3 toEyeVector = normalize(vertexPosition - vec3 (0,0,0));
+  vec3 reflectionDirection = normalize(reflect (toLightVector.xyz, normal));
+  float factor2 = pow( max ( 0.0, dot (reflectionDirection, toEyeVector)), shininess);
+  vec3 specular = color * (factor2 * kS);
+
+  vec3 accumulatedColor = ambientColor + diffuseColor + specular;
+  gl_FragColor = vec4(accumulatedColor, 1.0);
 }
