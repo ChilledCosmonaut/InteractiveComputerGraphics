622a710733b886742b058287a844d4ca3dbc9716
Jonas <jonas.schindler@stud-mail.uni-wuerzburg.de>
2021-08-25 00:28:10 +0900

    Refactored to resemble a node

--- src/rayvisitor.ts
+++ src/rayvisitor.ts
@@ -7,7 +7,7 @@ import Visitor from './visitor';
 import phong from './phong';
 import {
   Node, GroupNode, SphereNode,
-  AABoxNode, TextureBoxNode, PyramidNode
+  AABoxNode, TextureBoxNode, PyramidNode, ObjNode
 } from './nodes';
 import AABox from './aabox';
 import {Transformation} from "./transformation";
@@ -178,4 +178,11 @@ export default class RayVisitor implements Visitor {
    * @param node The node to visit
    */
   visitPyramidNode(node: PyramidNode) { }
+
+
+  /**
+   * Visits a textured box node
+   * @param node The node to visit
+   */
+  visitObjNode(node: ObjNode) { }
 }
